80f2a8fb8edd222b08539ac8791d5ea39555b03e
Hammad-ul-Hassan <103960660+HammadulHassan5812@users.noreply.github.com>
2024-12-05 01:58:28 +0500

    initial commit

---
+++ src/main/java/Server/main/Main.java
@@ -0,0 +1,8 @@
+package Server.main;
+
+public class Main
+{
+    public static void main(String[]args)
+    {}
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.Client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.Client to javafx.fxml;
+    exports com.example.Client;
+}
